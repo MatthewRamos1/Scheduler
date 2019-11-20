b8d0b597225f437d6dc4759dd33a87e998729ef6
Matthew Ramos <mmramos309@gmail.com>
2019-11-20 12:33:46 -0500

    Set vc as text field delegate, implemented func to remove keyboard after return

--- Scheduler/CreateEventViewController.swift
+++ Scheduler/CreateEventViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class CreateEventViewController: UIViewController {
 
+     @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.delegate = self
+        
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension CreateEventViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-    */
-
+    
 }
